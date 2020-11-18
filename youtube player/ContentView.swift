9229aa1785cae66fe9ca4a0d34c8e1a77e93e8ec
Susumu Hoshikawa <hoshi005@me.com>
2020-11-18 10:12:15 +0900

    feat: プレイヤーのサイズをそれっぽく調整

--- youtube player/ContentView.swift
+++ youtube player/ContentView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerSize: CGSize = .zero
+    
     var body: some View {
-        PlayerView()
+        VStack {
+            PlayerView()
+                .frame(
+                    width: playerSize.width,
+                    height: playerSize.height
+                )
+        }
+        .onAppear {
+            // windowサイズからplayerサイズを算出.
+            let frame = UIApplication.shared.windows.first?.frame ?? .zero
+            
+            // 縦横比が16:9になるように高さを調整.
+            playerSize = CGSize(
+                width: frame.width,
+                height: frame.width / 16 * 9
+            )
+        }
     }
 }
 
