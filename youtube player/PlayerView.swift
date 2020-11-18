7ecbd93071503d62f7bb003c8fab17438d64e57a
Susumu Hoshikawa <hoshi005@me.com>
2020-11-18 10:04:01 +0900

    feat: UIViewRepresentableで Player View を作成

---
+++ youtube player/PlayerView.swift
@@ -0,0 +1,36 @@
+//
+//  PlayerView.swift
+//  youtube player
+//
+//  Created by Susumu Hoshikawa on 2020/11/18.
+//
+
+import SwiftUI
+import YoutubeKit
+
+struct PlayerView: UIViewRepresentable {
+    
+    typealias UIViewType = YTSwiftyPlayer
+    
+    func makeUIView(context: Context) -> YTSwiftyPlayer {
+        // 初期化処理・初期設定.
+        let player = YTSwiftyPlayer()
+        player.autoplay = true
+        return player
+    }
+    
+    func updateUIView(_ uiView: YTSwiftyPlayer, context: Context) {
+        // 再生する動画の設定.
+        uiView.setPlayerParameters([
+            .playsInline(true),
+            .videoID("QW2TfV20FXY"),
+        ])
+        uiView.loadPlayer()
+    }
+}
+
+struct PlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerView()
+    }
+}
